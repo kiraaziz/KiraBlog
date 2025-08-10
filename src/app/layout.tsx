f99ba9a5d0f33da19d535c1e8d201a54cf4f83cb
kiraaziz <kiraaziz2020@gmail.com>
2025-08-10 19:05:47 +0000

    add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Suspense } from "react";
 import { Toaster } from "sonner";
 import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/next"
 
 export const dynamic = 'force-dynamic';
 
@@ -59,6 +60,7 @@ export default function RootLayout({
           {children}
         </Suspense>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
