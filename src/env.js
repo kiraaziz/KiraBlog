bba7e63aad9dd802111ae14932fb39f157f4b237
kiraaziz <kiraaziz2020@gmail.com>
2025-08-11 18:11:19 +0000

    migaret to postgresl

--- src/env.js
+++ src/env.js
@@ -19,10 +19,6 @@ export const env = createEnv({
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
    */
-  client: {
-    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(20),
-  },
 
   /**
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
@@ -32,8 +28,6 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     ADMIN_SECRET: process.env.ADMIN_SECRET,
     NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
